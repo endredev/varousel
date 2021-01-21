96a702acb1069cf32ef77f1bd1cc0926234ae163
Endredevc <dev.szabo.endre@gmail.com>
2021-01-21 22:21:32 +0100

    init view part

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <carousel>
+      <slide v-for="fruit in slotsMock" :key="fruit">
+        <h1>{{ fruit }}</h1>
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Carousel from "./components/Carousel.vue";
+import Slide from "./components/Slide.vue";
+
+@Component({
+  data() {
+    return {
+      slotsMock: ["Apple", "Orange", "Banana"]
+    };
+  },
+  components: {
+    Carousel,
+    Slide
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Slide.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="slide">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Slide"
+})
+export default class Slide extends Vue {}
+</script>
+
+<style lang="scss">
+.slide {
+  min-height: 200px;
+  min-width: 400px;
+  border: 1px solid #c1c1c1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
